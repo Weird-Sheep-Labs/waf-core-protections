cb0d693f83b9ed34a2ca620b3e1b181c50733bd5
Armand Rego <armand@weirdsheeplabs.com>
2024-04-28 22:19:26 +0100

    Update construct ID.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from waf_core_protections.waf_core_protections_stack import WAFCoreProtectionsSt
 app = cdk.App()
 WAFCoreProtectionsStack(
     app,
-    "WafCoreProtectionsStack",
+    "WAFCoreProtectionsStack",
     env=cdk.Environment(
         account=os.environ.get("CDK_DEPLOY_ACCOUNT", os.environ["CDK_DEFAULT_ACCOUNT"]),
         region=os.environ.get("CDK_DEPLOY_REGION", os.environ["CDK_DEFAULT_REGION"]),
